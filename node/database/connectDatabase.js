28d24f7af0c82633399437faad477fa69b18b3b9
GOvind MADdala <99980734+govindmaddala@users.noreply.github.com>
2024-04-10 14:27:21 +0530

    database setup

--- node/database/connectDatabase.js
+++ node/database/connectDatabase.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 exports.dbConnect = async () => {
     try {
         // console.log("process.env.MONGO_SERVER",process.env.MONGO_LOCAL, process.env.MONGO_SERVER)
-        const conn = await mongoose.connect(process.env.MONGO_LOCAL)
+        const conn = await mongoose.connect(process.env.MONGO_SERVER)
         console.log(`Mongodb connected on ${conn.connection.host}`)
     } catch (error) {
         console.log(`Mongo Error: ${error.message}`)
